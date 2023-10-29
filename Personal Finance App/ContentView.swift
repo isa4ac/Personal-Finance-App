9eb9bd59393ddb94fda402c0541019ba242953a5
Isaac Van Meter <ivanmeter28@gmail.com>
2023-10-29 14:39:01 -0400

    Initial Commit

---
+++ Personal Finance App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Personal Finance App
+//
+//  Created by Isaac Vanmeter on 10/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Personal Finance App/Personal_Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Finance_AppApp.swift
+//  Personal Finance App
+//
+//  Created by Isaac Vanmeter on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
